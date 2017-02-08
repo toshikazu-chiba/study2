571929475ffcf394bfdf38625403c66953002d0c
Toshikazu Chiba <chiba@keis-software.com>
2017-02-08 18:11:56 +0900

    Sp slide menu was completed

--- common/js/action.js
+++ common/js/action.js
@@ -1,3 +1,19 @@
-var slideMenu = new SlideMenu('.sp_menu > .inner > a', '.sp_menu_content', '250px');
 
-slideMenu.run();
+$(function () {
+	var slideMenu = new SlideMenu('.sp_menu a');
+	slideMenu.run();
+});
+
+$(function () {
+	var constructor1 = new Constructor('.test1', 'hello1');
+	var constructor2 = new Constructor('.test2', 'hello2');
+	var constructor3 = new Constructor('.test3', 'hello3');
+	var constructor4 = new Constructor('.test4', 'hello4');
+	var constructor5 = new Constructor('.test5', 'hello5');
+
+	constructor1.test1();
+	constructor2.test2();
+	constructor3.test3();
+	constructor4.test4();
+	constructor5.test5();
+});
--- common/js/common.js
+++ common/js/common.js
@@ -1,104 +1,143 @@
-$(function () {
+// オーバーレイヤー
+var Overlayer = function (actionType, target) {
+	// アクティブ・非アクティブの設定
+	this.actionType = actionType;
+	this.target = target;
+}
 
-	// スマホ:スライドメニュー
-	var SlideMenu = function (handler, content, contentWidth) {
-		// イベントハンドラー
-		this.handler = $(handler);
-		// スライドメニューのセレクタ
-		this.content = $(content);
-		// スライドメニューの幅
-		this.contentWidth = contentWidth;
-		// オーバーレイヤーを開く
-		this.openOverlayer = new Overlayer(this.handler, 'open');
-		// オーバーレイヤーを閉じる
-		this.closeOverlayer = new Overlayer(this.handler, 'close');
+// イベントのアクティブ・非アクティブ状態のフラグ
+Overlayer.prototype.activeFlg = false;
+// オーバーレイヤーのセレクター
+Overlayer.prototype.currentDom = '.common_overlayar';
+
+Overlayer.prototype.run = function () {
+	Overlayer.prototype.mainProcess();
+}
+Overlayer.prototype.mainProcess = function () {
+	// オーバーレイヤーを開く
+	if (!Overlayer.prototype.activeFlg) {
+		Overlayer.prototype.open();
+	// オーバーレイヤーを閉じる
+	} else if (Overlayer.prototype.activeFlg) {
+		Overlayer.prototype.close();
 	}
+}
 
-	SlideMenu.prototype = {
-		// イベントのアクティブ・非アクティブ状態のフラグ
-		activeFlg: false
-		// スライドの移動量
-		,slideVolume: this.contentWidth
-		// スライドスピード
-		,slideSpeed: 500
+// オーバーレイヤーを開く
+Overlayer.prototype.open = function () {
+	$(Overlayer.prototype.currentDom).addClass('active');
+	Overlayer.prototype.activeFlg = true;
+}
 
-		,run: function () {
-			SlideMenu.prototype.mainProcess();
-		}
+// オーバーレイヤーを閉じる
+Overlayer.prototype.close = function () {
+	$(Overlayer.prototype.currentDom).removeClass('active');
+	Overlayer.prototype.activeFlg = false;
+}
 
-		,mainProcess: function () {
-			this.handler.on('click', function () {
-				// スライドを開く
-				if (!SlideMenu.prototype.activeFlg) {
-					SlideMenu.prototype.open();
-				// スライドを閉じる
-				} else if (SlideMenu.prototype.activeFlg) {
-					SlideMenu.prototype.open();
-				}
-			});
-		}
+// スマホ:スライドメニュー
+var SlideMenu = function (handler) {
+	// イベントハンドラー
+	this.handler = handler;
+}
 
-		// スライドを開く
-		,open: function () {
-			SlideMenu.prototype.content.animate({
-				right: 0
-			}, SlideMenu.prototype.slideSpeed);
-			SlideMenu.prototype.openOverlayer;
-			SlideMenu.prototype.activeFlg = true;
-		}
+// イベントのアクティブ・非アクティブ状態のフラグ
+SlideMenu.prototype.activeFlg = false;
+// スライドメニューのセレクタ
+SlideMenu.prototype.content = '.sp_menu_item';
+// 閉じるボタン
+SlideMenu.prototype.colseBtn = '.sp_menu_colse_btn';
+// スライドの移動量
+SlideMenu.prototype.slideVolume = '-250px';
+// スライドスピード
+SlideMenu.prototype.slideSpeed = 500;
+SlideMenu.prototype.slideMenuOpenOverlayer = new Overlayer('open');
+SlideMenu.prototype.slideMenuCloseOverlayer = new Overlayer('close', SlideMenu.prototype.close);
 
-		// スライドを閉じる
-		,close: function () {
-			SlideMenu.prototype.content.animate({
-				right: SlideMenu.prototype.slideVolume
-			}, SlideMenu.prototype.slideSpeed);
-			SlideMenu.prototype.closeOverlayer;
-			SlideMenu.prototype.activeFlg = false;
-		}
-	};
 
-	// オーバーレイヤー
-	var Overlayer = function (handler, actionType) {
-		// イベントハンドラー
-		this.handler = $(handler);
-		// アクティブ・非アクティブの設定
-		this.actionType = actionType;
+SlideMenu.prototype.run = function () {
+	$(this.handler).on('click', function () {
+		SlideMenu.prototype.open();
+		SlideMenu.prototype.slideMenuOpenOverlayer.run();
+	});
+	$('a', SlideMenu.prototype.colseBtn).on('click', function () {
+		SlideMenu.prototype.close();
+		SlideMenu.prototype.slideMenuCloseOverlayer.run();
+	});
+}
+
+// SlideMenu.prototype.mainProcess = function () {
+// 	// スライドを開く
+// 	if (!SlideMenu.prototype.activeFlg) {
+// 		SlideMenu.prototype.open();
+// 		SlideMenu.prototype.slideMenuOpenOverlayer.run();
+// 	// スライドを閉じる
+// 	} else if (SlideMenu.prototype.activeFlg) {
+// 		SlideMenu.prototype.close();
+// 		SlideMenu.prototype.slideMenuCloseOverlayer.run();
+// 	}
+// }
+
+// スライドを開く
+SlideMenu.prototype.open = function () {
+	$(this.content).animate({ 
+		right: 0
+	}, SlideMenu.prototype.slideSpeed);
+	$(SlideMenu.prototype.colseBtn).addClass('active');
+	SlideMenu.prototype.activeFlg = true;
+}
+
+// スライドを閉じる
+SlideMenu.prototype.close = function () {
+	$(this.content).animate({ 
+		right: SlideMenu.prototype.slideVolume
+	}, SlideMenu.prototype.slideSpeed);
+	$(SlideMenu.prototype.colseBtn).removeClass('active');
+	SlideMenu.prototype.activeFlg = false;
+}
+
+// サンプル
+var Constructor = function (handler, comment) {
+		this.handler = handler;
+		this.comment = comment;
+}
+
+Constructor.prototype = {
+	content: '.test5_content'
+	,flg: false
+	,test1: function () {
+		console.log (this.comment);
 	}
-
-	Overlayer.prototype = {
-
-		// イベントのアクティブ・非アクティブ状態のフラグ
-		activeFlg: false
-		// オーバーレイヤーのセレクター
-		,currentDom: $('.overlayer')
-
-		,run: function () {
-			Overlayer.prototype.mainProcess();
-		}
-		,mainProcess: function () {
-
-			this.handler.on('click', function () {
-				// オーバーレイヤーを開く
-				if (!SlideMenu.prototype.activeFlg) {
-					Overlayer.prototype.open();
-				// オーバーレイヤーを閉じる
-				} else if (SlideMenu.prototype.activeFlg) {
-					Overlayer.prototype.open();
-				}
-			});
-		}
-
-		// オーバーレイヤーを開く
-		,open: function () {
-			$(this).removeClass('active');
-			Overlayer.prototype.activeFlg = true;
-		}
-
-		// オーバーレイヤーを閉じる
-		,close: function () {
-			$(this).removeClass('active');
-			Overlayer.prototype.activeFlg = false;
-		}
+	,test2: function () {
+		console.log ($(this.handler));
+		console.log (this.comment);
 	}
-});
-
+	,test3: function () {
+		$(this.handler).text (this.comment);
+	}
+	,test4: function () {
+		$(this.handler).on('click', function () {
+			console.log('test4');
+		});
+	}
+	,test5: function () {
+		$(this.handler).on('click', function () {
+			switch (Constructor.prototype.flg) {
+				case false:
+					$(Constructor.prototype.content).addClass('active');
+					$(Constructor.prototype.content).animate ({
+						height: '300px'
+					}, 500);
+					Constructor.prototype.flg = true;
+					break; 
+				case true:
+					$(Constructor.prototype.content).animate ({
+							height: 0
+					}, 500);
+					$(Constructor.prototype.content).removeClass('active');
+					Constructor.prototype.flg = false;
+					break; 
+			}
+		});
+	}
+}
